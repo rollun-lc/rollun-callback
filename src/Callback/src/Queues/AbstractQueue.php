0514c95e0277cba9e87f94a23cd6639bc7706b00
victorynox <it.professor02@gmail.com>
2018-02-19 16:28:45 +0200

    Add new Queue add support amazon sqs queue

---
+++ src/Callback/src/Queues/AbstractQueue.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 18.10.17
+ * Time: 18:28
+ */
+
+namespace rollun\callback\Queues;
+
+use ReputationVIP\QueueClient\PriorityHandler\StandardPriorityHandler;
+use ReputationVIP\QueueClient\QueueClient;
+use ReputationVIP\QueueClient\QueueClientInterface;
+
+abstract class AbstractQueue implements QueueInterface
+{
+    /** @var QueueClient */
+    protected $queueClient;
+
+    /** @var int */
+    protected $delaySeconds;
+
+    /** @var string */
+    protected $queueName;
+
+    /** @var string */
+    protected $priorityHandlerClass;
+
+    /**
+     * AbstractQueue constructor.
+     * @param $queueName
+     * @param int $delaySeconds
+     * @param string $priorityHandlerClass
+     */
+    abstract public function __construct(
+        $queueName,
+        $delaySeconds = 0,
+        $priorityHandlerClass = StandardPriorityHandler::class
+    );
+
+    /**
+     * Check if queue is empty.
+     * @return bool
+     */
+    public function isEmpty()
+    {
+        return $this->queueClient->isEmpty($this->queueName);
+    }
+
+    /**
+     * @param null $priority
+     * @return null|\rollun\callback\Queues\Message
+     */
+    public function getMessage($priority = null)
+    {
+        $messages = $this->queueClient->getMessages($this->queueName, 1, $priority);
+        if (isset($messages[0])) {
+            $message = new Message($messages[0]);
+            $this->queueClient->deleteMessage($this->queueName, $messages[0]);
+        } else {
+            $message = null;
+        }
+        return $message;
+    }
+
+    /**
+     * @param $message
+     * @param null $priority
+     * @return QueueInterface
+     */
+    public function addMessage($message, $priority = null)
+    {
+        $this->queueClient->addMessage($this->queueName, $message, $priority, $this->delaySeconds);
+        return $this;
+    }
+
+    /**
+     * @param $queueName
+     * @param null $priority
+     * @return QueueInterface
+     */
+    public function purgeQueue($queueName, $priority = null)
+    {
+        $this->queueClient->purgeQueue($queueName, $priority);
+        return $this;
+    }
+
+    /**
+     * Return queue name;
+     * @return string
+     */
+    public function getName() {
+        return $this->queueName;
+    }
+
+}
