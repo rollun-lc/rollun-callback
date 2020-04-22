5d17b37d5d24d0878a95f8400dc29cfbc4319a16
Yurii Orlyk <orlyk.rollun@gmail.com>
2020-04-22 13:43:45 +0300

    [1556] refactoring interrupter middleware

--- src/Callback/src/Middleware/InterrupterMiddleware.php
+++ src/Callback/src/Middleware/InterrupterMiddleware.php
@@ -60,7 +60,14 @@ class InterrupterMiddleware implements MiddlewareInterface
             $value = $request->getAttribute(AbstractParamsResolver::ATTRIBUTE_WEBHOOK_VALUE);
             $result = call_user_func($callable, $value);
 
-            $statusCode = $result instanceof PayloadInterface ? 202 : 200;
+            if ($result instanceof PayloadInterface) {
+                $statusCode = 202;
+            } elseif (is_array($result) && !empty($result['error'])) {
+                $statusCode = 500;
+            } else {
+                $statusCode = 200;
+            }
+
         } catch (\Throwable $t) {
             $result = ['error' => $t->getMessage()];
 
