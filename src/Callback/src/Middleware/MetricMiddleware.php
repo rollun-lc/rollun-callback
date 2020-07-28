fdf0eee00b47053389511a8e47cce34aba09e4a4
rratsunrollun <r.ratsun.rollun@gmail.com>
2020-07-28 17:08:24 +0300

    cron metrics to prometheus

--- src/Callback/src/Middleware/MetricMiddleware.php
+++ src/Callback/src/Middleware/MetricMiddleware.php
@@ -8,6 +8,7 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Psr\Log\LoggerInterface;
+use rollun\logger\Writer\PrometheusWriter;
 
 /**
  * Class MetricMiddleware
@@ -37,21 +38,28 @@ class MetricMiddleware implements MiddlewareInterface
     }
 
     /**
+     * @throws \ReflectionException
+     */
+    public function __wakeup()
+    {
+        InsideConstruct::initWakeup(['logger' => LoggerInterface::class]);
+    }
+
+    /**
      * @inheritDoc
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         if ($request->getUri()->getPath() == '/api/webhook/cron' && $request->getMethod() == 'GET') {
-            $serviceName = getenv('SERVICE_NAME');
-            if (!empty($serviceName)) {
-                $this
-                    ->logger
-                    ->notice(
-                        'METRICS', [
-                            'metricId' => str_replace('-', '_', $serviceName) . '__webhook_cron_get__metric',
-                            'value'    => 1
-                        ]
-                    );
+            if (!empty(getenv('SERVICE_NAME'))) {
+                // prepare metric data
+                $metricData = [
+                    PrometheusWriter::METRIC_ID => 'webhook_cron',
+                    PrometheusWriter::VALUE     => 1
+                ];
+
+                // send metric
+                $this->logger->notice('METRICS_COUNTER', $metricData);
             }
         }
 
